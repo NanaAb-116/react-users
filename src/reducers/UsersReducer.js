30fe28bc764c95fd335635b3a0d4a8fdc729b154
NanaAb-116 <nanaabboateng@gmail.com>
2022-08-30 14:00:13 +0000

    install redux

---
+++ src/reducers/UsersReducer.js
@@ -0,0 +1,12 @@
+let initialState = {
+  users: [
+    { name: "Terry", email: "terry@gmail.com", gen: 22, id: "1659431731207" },
+    { name: "Ash", email: "ash@gmail.com", gen: 25, id: "1659431734534" },
+  ],
+};
+
+let UsersReducers = (state = initialState, action) => {
+  return state;
+};
+
+export default UsersReducers;
